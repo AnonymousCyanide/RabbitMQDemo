45a88c74d702695942a949a8f3e5965de7fbaf77
Avi <avidubey2000@gmail.com>
2021-05-05 20:11:24 +0530

    Made a class rabbitMQ in sender

--- sender.py
+++ sender.py
@@ -2,3 +2,27 @@ try :
     import pika
 except Exception as e :
     print("Some module is missing {}".format_map(e))
+class RabbitMQ(object):
+    def __init__(self, q ='hello'):
+        self._connection = pika.BlockingConnection(
+          pika.ConnectionParameters(host='localhost'))
+        self._channel = self._connection.channel()
+        self._channel.queue_declare(queue=q)
+    def publish(self , payload={}):
+        self._channel.basic_publish(exchange='',
+        routing_key ='Hi',
+        body = str(payload))
+        print('Message published')
+        self._connection.close()
+
+if __name__ == '__main__':
+    server = RabbitMQ(q='Hi')
+    server.publish(payload={'data':'This is the data'})
+
+
+
+
+
+
+
+
